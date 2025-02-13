d3894e48125b2e2a2557d7263b669fb335ae7fe2
anthonysfarias <akileslive.farias10@gmail.com>
2025-02-13 14:03:51 -0300

    refactor: reorganizar lógica da main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { NgModuleRef } from '@angular/core';  // Importando de '@angular/core'
 import { AppModule } from './app/app.module';
 
+/**
+ * Inicializa a aplicação Angular com o módulo especificado.
+ * @returns {Promise<void | NgModuleRef<AppModule>>} Uma promessa que é resolvida quando o módulo é inicializado com sucesso, 
+ *         ou resolve para void em caso de falha.
+ */
+function bootstrapApp(): Promise<void | NgModuleRef<AppModule>> {
+  return platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err: any) => {
+      // Registra o erro no console
+      console.error('Erro ao inicializar a aplicação:', err);
+    });
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
- 
+// Chama a função para inicializar a aplicação
+bootstrapApp();
