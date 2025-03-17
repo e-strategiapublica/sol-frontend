9f261ffa1fa240a8c5e92a54d522338271ba6bc4
anthonysfarias <akileslive.farias10@gmail.com>
2025-03-17 09:50:40 -0300

    refactor: API consumption logic and improve request

--- src/services/allotments.service.ts
+++ src/services/allotments.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map, catchError } from 'rxjs';
-import { CategoryRequestDto } from 'src/dtos/category/category-request.dto';
-import { CategoryResponseDto } from 'src/dtos/category/category-response.dto';
 import { environment } from 'src/environments/environment';
 import { BaseService } from './base.service';
 import { AllotmentResponseDto } from 'src/dtos/allotment/allotment-response.dto';
@@ -11,7 +9,7 @@ import { AllotmentResponseDto } from 'src/dtos/allotment/allotment-response.dto'
   providedIn: 'root'
 })
 export class AllotmentsService extends BaseService {
-  private url = `${environment.api.path}/allotment`;
+  private url = `${environment.api.path}/allotments`;
 
   constructor(
     private httpClient: HttpClient
@@ -19,23 +17,21 @@ export class AllotmentsService extends BaseService {
     super();
   }
 
-  getAllotments(): Observable<any> {
+  getAllotments(): Observable<AllotmentResponseDto[]> {
     return this.httpClient
-      .post(`${this.url}/register`, this.authorizedHeader)
-      .pipe(map(response => response), catchError(this.serviceError))
+      .get<AllotmentResponseDto[]>(`${this.url}`, this.authorizedHeader)
+      .pipe(map(response => response), catchError(this.serviceError));
   }
 
-  getAllotmentsById(Id: string): Observable<any> {
+  getAllotmentsById(id: string): Observable<AllotmentResponseDto> {
     return this.httpClient
-      .get(`${this.url}/allotment-by-id/${Id}`, this.authorizedHeader)
-      .pipe(map(response => response), catchError(this.serviceError)
-      );
+      .get<AllotmentResponseDto>(`${this.url}/${id}`, this.authorizedHeader)
+      .pipe(map(response => response), catchError(this.serviceError));
   }
 
-  download(_id: string) {
+  download(id: string): Observable<Blob> {
     return this.httpClient
-      .post(`${this.url}/download-file-by-id/${_id}`, null, this.authorizedHeaderBlob)
-      .pipe(map(response => response), catchError(this.serviceError))
+      .get(`${this.url}/${id}/file`, { ...this.authorizedHeaderBlob, responseType: 'blob' })
+      .pipe(map(response => response), catchError(this.serviceError));
   }
-
 }
