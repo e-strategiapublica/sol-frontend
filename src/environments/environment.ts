2528efe46d72893ecabe9769d0fb3bd3222948e7
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-04-07 07:59:48 -0300

    chore: update environment configuration for API server settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
 export const environment = {
   production: true,
-  encrypt_key: '5708c8d4-0b2a-4cbd-bea4-99981079020a',
-  // api: {
-  //   server: 'http://localhost:4003',
-  //   port: '',
-  //   path: 'http://localhost:4003',
-  // }
+  encrypt_key: "5708c8d4-0b2a-4cbd-bea4-99981079020a",
   api: {
-    server: 'https://api.homologacao.solpublico.org',
-    port: '',
-    path: 'https://api.homologacao.solpublico.org',
-  }
+    server: "207.246.127.17/api",
+    port: "",
+    path: "207.246.127.17/api",
+  },
+  // api: {
+  //   server: 'https://api.homologacao.solpublico.org',
+  //   port: '',
+  //   path: 'https://api.homologacao.solpublico.org',
+  // }
 };
-
