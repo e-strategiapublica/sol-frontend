1bd30b615d5fd43fb566aef71d97fa7bf6eb7a94
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2025-04-08 05:40:30 -0300

    chore: clean up commented-out API server configurations in production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,8 @@ export const environment = {
   production: true,
   encrypt_key: "5708c8d4-0b2a-4cbd-bea4-99981079020a",
   api: {
-    server: "207.246.127.17/api",
+    server: "http://207.246.127.17",
     port: "",
-    path: "207.246.127.17/api",
+    path: "api",
   },
-  // api: {
-  //   server: 'https://api.homologacao.solpublico.org',
-  //   port: '',
-  //   path: 'https://api.homologacao.solpublico.org',
-  // }
 };
