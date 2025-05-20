937288257ed809a58dae12fa713bcf1d7a1e208d
anthonysfarias <akileslive.farias10@gmail.com>
2025-05-20 03:05:48 -0300

    fix: handle unresponsive behavior on draft save and incorrect message on reload (#34)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,9 @@
 export const environment = {
   production: true,
   encrypt_key: "5708c8d4-0b2a-4cbd-bea4-99981079020a",
-  // api: {
-  //   server: "http://207.246.127.17",
-  //   port: "",
-  //   path: "api",
-  // },
-   api: {
-    server: 'http://localhost:4003',
-    port: '',
-    path: 'http://localhost:4003',
-  }
+  api: {
+    server: "http://207.246.127.17",
+    port: "",
+    path: "api",
+  },
 };
